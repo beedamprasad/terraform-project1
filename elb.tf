4c4e12329ee3dd13a8e3cac71cc77fb183093e5d
beedamprasad <136710205+beedamprasad@users.noreply.github.com>
2024-01-10 11:43:35 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["eu-west-3a", "eu-west-3b"]
+  name               = "Prasad-terraform-elb"
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "prasad-tf-elb"
   }
 }
 
@@ -29,17 +29,17 @@ resource "aws_elb" "bar" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["eu-west-3a", "eu-west-3b"]
+  availability_zones      = ["ap-south-1a", "ap-south-1b"]
   database_name           = "mydb"
-  master_username         = "raham"
-  master_password         = "Rahamshaik#444555"
+  master_username         = "Prasad
+  master_password         = "Prasad#444555"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }*/
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-08fe36427228eddc4"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
